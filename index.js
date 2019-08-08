ff7cff3dd8e72f1c317067303970e1aa582023e6
uu4k <t.shimizu1115@gmail.com>
2019-08-09 07:47:53 +0900

    upload image to s3.

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 require('dotenv').config()
-
+const dateformat = require('dateformat')
 const vision = require('@google-cloud/vision')
-const client = new vision.ImageAnnotatorClient()
+const { Storage } = require('@google-cloud/storage')
 
 const PLASTIC_BOTTLE_MID = '/m/02rlncx'
+const CAMERA_OUTPUT_PATH = `${__dirname}/refuse.jpg`
 
 const PiCamera = require('pi-camera')
 const myCamera = new PiCamera({
   mode: 'photo',
-  output: `${__dirname}/refuse.jpg`,
+  output: CAMERA_OUTPUT_PATH,
   width: 640,
   height: 480,
   nopreview: true
 })
 
+const storage = new Storage({
+  projectId: process.env.PROJECT_ID
+})
+const bucketName = process.env.BUCKET_NAME
+
+const client = new vision.ImageAnnotatorClient()
+
 myCamera
   .snap()
   .then(result => {
+    // 画像をGCSにアップロード
+    const now = new Date()
+    storage
+      .bucket(bucketName)
+      .upload(CAMERA_OUTPUT_PATH, {
+        destination: `${dateformat(new Date(), 'yyyymmdd/hhMMss')}.jpg`,
+        gzip: true
+      })
+      .then(res => {
+        console.info(`${CAMERA_OUTPUT_PATH} uploaded to ${bucketName}.`)
+      })
+      .catch(err => {
+        console.error('ERROR:', err)
+      })
+
     client
       .labelDetection(`${__dirname}/refuse.jpg`)
       .then(results => {
@@ -37,7 +60,7 @@ myCamera
         // TODO 自分のslackに通知
       })
   })
-  .catch(error => {
-    console.error('ERROR:', error)
+  .catch(err => {
+    console.error('ERROR:', err)
     // TODO 自分のslackに通知
   })
